2d7cdda2c11668d22d92d1f35a2e5241aa388907
tuvshuu <tuvshee2k@gmail.com>
2024-03-28 21:20:01 +0800

    FoodCard Updated
    Added Hover Effect mar.28

--- frontend/src/components/layout/footer/index.tsx
+++ frontend/src/components/layout/footer/index.tsx
@@ -28,15 +28,15 @@ const LinkItem: React.FC<LinkItemProps> = ({ text, href }) => {
 };
 
 const linkItems = [
-     { text: "Нүүр", href: "@/components/pages/layout/index" },
-     { text: "Холбоо барих", href: "@/components/pages/layout/contact" },
-     { text: "Хоолны цэс", href: "@/components/pages/layout/menu" },
-     { text: "Үйлчилгээний нөхцөл", href: "@/components/pages/layout/terms" },
+     { text: "Нүүр", href: "/layout/index" },
+     { text: "Холбоо барих", href: "/layout/contact" },
+     { text: "Хоолны цэс", href: "/layout/menu" },
+     { text: "Үйлчилгээний нөхцөл", href: "/layout/terms" },
      {
           text: "Хүргэлтийн бүс",
-          href: "@/components/pages/layout/delivery-zone",
+          href: "/layout/delivery-zone",
      },
-     { text: "Нууцлалын бодлого", href: "@/components/pages/layout/privacy" },
+     { text: "Нууцлалын бодлого", href: "/layout/privacy" },
 ];
 
 const Footer: React.FC = () => {
