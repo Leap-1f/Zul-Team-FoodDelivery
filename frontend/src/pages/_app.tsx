6743bf41e84a6e931f58c8eb868e5690de7c3489
Zulsar Khasarkhuu <23LP5930@G401-CC0100.local>
2024-03-29 16:55:59 +0800

    3/29 header vh hereg bolloo

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Header } from "@/components/layout/header";
+import { Box } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Box style={{height: "100vh"}}>
+  <Header></Header>
+   <Component {...pageProps} />
+   </Box>
+ 
 }
